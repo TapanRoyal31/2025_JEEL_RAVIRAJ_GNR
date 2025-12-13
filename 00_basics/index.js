7ff68a260d1333232b79b0ced0d0632983bed062
TapanRoyal31 <tapanr3105@gmail.com>
2025-12-13 14:04:53 +0530

    alert & parseInt & prompt

--- 00_basics/index.js
+++ 00_basics/index.js
@@ -1,1 +1,17 @@
-document.write(' technosoft');
+// document.write(' technosoft');
+
+// var a = 10;
+// var b = 5; 
+
+// alert('Addition :' + (a+b)); //15
+// alert('Addition :'+ a+b); // 105
+// console.log("addition",a+b);//15
+
+// let a = prompt('Enter the Number :'); // prompt in js == scanf in c
+// let b = prompt('Enter 2nd Number :'); // prompt by default value string store
+
+
+let a = parseInt(prompt('Enter the number :')); // parseInt(string -> number convert)  
+let b = parseInt(prompt('Enter 2nd number :')); 
+
+console.log('a and b addition : ',(a + b));
