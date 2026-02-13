9b7cb1e561da987119d9abdf3c85b521afabc56c
TapanRoyal31 <tapanr3105@gmail.com>
2026-02-13 15:58:36 +0530

    "strings"

---
+++ 04_String/string.js
@@ -0,0 +1,11 @@
+        // 01234567890123456
+let abc = '       qwerty    ';
+
+// let result = abc[0]; // q
+// let result = abc.length(); //17
+// let result = abc.trimStart(); // 10
+// let result = abc.trimEnd(); // 13
+// let result = abc.trim(); // 6
+
+console.log(result);
+console.log(abc);
