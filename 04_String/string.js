3accba7e809f389e6de596321e4140a48f4b741b
TapanRoyal31 <tapanr3105@gmail.com>
2026-02-14 12:54:41 +0530

    "string methods"

--- 04_String/string.js
+++ 04_String/string.js
@@ -1,11 +1,21 @@
         // 01234567890123456
-let abc = '       qwerty    ';
+// let abc = 'qwerty@gmail.com';
 
 // let result = abc[0]; // q
 // let result = abc.length(); //17
 // let result = abc.trimStart(); // 10
-// let result = abc.trimEnd(); // 13
+// let result = abc.trimEnd(); // 13    
 // let result = abc.trim(); // 6
 
-console.log(result);
-console.log(abc);
+// let result = abc.endsWith('@gmail.com'); // true
+// let result = abc.includes('qwerty'); // true
+// let result = abc.concat(' pass');
+// let result = abc.indexOf('q');// 7
+// let result = abc.replace('@gmail.com','@yahoo.com');
+// let result = abc.charCodeAt(7); // 113
+// let result = abc.toUpperCase() //
+// let result = abc.toLowerCase() //
+// let result = abc.substring(6,12); // @gmail
+
+// console.log(result);
+// console.log(abc);
