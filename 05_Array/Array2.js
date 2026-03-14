5f9087594c4dec9a919f8899669b13cb3c70e508
TapanRoyal31 <tapanr3105@gmail.com>
2026-03-14 14:26:44 +0530

    foreach

---
+++ 05_Array/Array2.js
@@ -0,0 +1,31 @@
+// let arr = [1, 2, 3, 4, 5];
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//     const element = arr[i];
+//     sum += element; // sum = sum + element
+// }
+
+// console.log(sum);
+
+//--------------------------------------------------------------------------------
+
+// let arr = [1, 2, 3, 4, 5];
+
+// console.log("before loop",arr);
+
+// for (let i = 0; i < arr.length; i++) {
+//     if(i == (arr.length - 1)){
+//         arr[i] = arr[i] + (arr[i] + 1);
+//     }
+//     else arr[i] = arr[i] + arr[i+1];
+// }
+
+// console.log("after loop",arr);
+
+//---------------------------------------------------------------------------------
+
+// let arr = [1, 2, 3, 4, 5];
+
+// arr.forEach(function(sujal){
+//     console.log(sujal + 10);
+// })
