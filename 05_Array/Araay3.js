9cba5dea6052b9a94c1bd360d4e4f4a6fb994009
TapanRoyal31 <tapanr3105@gmail.com>
2026-03-27 11:52:51 +0530

    number sorting

--- 05_Array/Araay3.js
+++ 05_Array/Araay3.js
@@ -8,16 +8,17 @@
 
 // console.log("reverse array :",revArr);
 
+// <------------------------------------Number Sorting---------------------------------------------->
 
-let arr = [10, 20 , 470 , 30, 50, 40];
+// let arr = [10, 20 , 470 , 30, 50, 40];
 
-let newArr = arr.sort(function(a, b) {
-  return a - b;
-});
+// let newArr = arr.sort(function(a, b) {
+//   return a - b;
+// });
 
-console.log("sorted arr: ",newArr);
+// console.log("sorted arr: ",newArr);
 
-let revArr = arr.sort(function(a, b){
-    return b-a;
-})
-console.log("reverse arr: ",revArr);
+// let revArr = arr.sort(function(a, b){
+//     return b-a;
+// })
+// console.log("reverse arr: ",revArr);
