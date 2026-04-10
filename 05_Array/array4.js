18c42c4ce7cc9e974aed69fd979f4662b6526b7b
TapanRoyal31 <tapanr3105@gmail.com>
2026-04-10 12:38:11 +0530

    task

---
+++ 05_Array/array4.js
@@ -0,0 +1,31 @@
+let arr = [-1, 3, 3, 6];
+// let some = arr.some((num) => num % 2 == 0);
+// console.log(some);
+
+// --------------------------------------------------------
+
+// let every = arr.every((num) => num > 0);
+// console.log(every);
+
+// ---------------------------------------------------------
+
+// let reduce = arr.reduce((acc,num) => acc + num);
+// console.log(reduce);
+
+//------------------------------------------------------------ TASK -----------------------------------------------------------
+/*
+1. Check if any number is negative (some)
+const nums = [3, 5, -2, 8];
+
+👉 Return true if any number is negative.
+Hint:
+Use num < 0
+
+2. Check if all strings are non-empty (every)
+const words = ["hello", "world", "js"];
+👉 Return true if all strings have length > 0.
+
+3. Sum of all numbers (reduce)
+const nums = [1, 2, 3, 4];
+👉 Find total sum.
+*/
